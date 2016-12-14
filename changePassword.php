171d671d068f59223fd7af0020f5fb3b087aec5d
sayali_bagul <bagulsayali4883@gmail.com>
2016-12-14 21:05:15 +0530

    Removing style tag

--- changePassword.php
+++ changePassword.php
@@ -23,6 +23,8 @@ $query2='select * from bb_info where bb_regno="'.$_SESSION['bb_regno'].'"  ';
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- DataTables -->
   <link rel="stylesheet" href="plugins/datatables/dataTables.bootstrap.css">
+   <!-- Table style -->
+   <link rel="stylesheet" href="dist/css/table.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
@@ -35,16 +37,7 @@ $query2='select * from bb_info where bb_regno="'.$_SESSION['bb_regno'].'"  ';
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <style>
-    #footer {
-    position:fixed;
-    right:0;
-    bottom:0;
-    margin:0;
-    width:10%;
-}
-#footer img {width:100%;}
-  </style>
+ 
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
