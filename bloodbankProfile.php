cd433763898f88a2d302cdb9d9a00530a469f965
sayali_bagul <bagulsayali4883@gmail.com>
2016-12-14 20:56:16 +0530

    Removing style tag

--- bloodbankProfile.php
+++ bloodbankProfile.php
@@ -38,16 +38,7 @@ $query2='select * from bb_info where bb_regno="'.$_SESSION['bb_regno'].'"  ';
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <style>
-    #footer {
-    position:fixed;
-    right:0;
-    bottom:0;
-    margin:0;
-    width:10%;
-}
-#footer img {width:100%;}
-  </style>
+ 
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
