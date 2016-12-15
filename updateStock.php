1a602e5825c08810624d589e3987049e7c915174
sayali_bagul <bagulsayali4883@gmail.com>
2016-12-15 12:40:04 +0530

    logo link

--- updateStock.php
+++ updateStock.php
@@ -186,7 +186,7 @@ $q=mysqli_query($con,"insert into bb_dailystock_curr (bb_regno,date,wb_a_pos,wb_
 
   <header class="main-header">
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="currentdailyStock.php" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini">Heroes</span>
       <!-- logo for regular state and mobile devices -->
